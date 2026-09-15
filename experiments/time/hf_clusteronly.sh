f6700f653f9f081c7769c8a0443b986d581b0ab7
jorisfu <joris.funk@student.hpi.uni-potsdam.de>
2026-09-15 12:02:54 +0200

    fishy hyperfine results

--- experiments/time/hf_clusteronly.sh
+++ experiments/time/hf_clusteronly.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 hyperfine \
-  --runs 5 \
+  --runs 50 \
   --warmup 3 \
-  -L dist euclidean,dixon_pds_sqeuclidean,nandist_euclidean \
+  -L dist euclidean,dixon_pds_sqeuclidean,nandist_euclidean,eirola_esd_mvn,eirola_esd_gmm,mesquita_eed \
   --export-json "./results/time/hyperfine-clusteronly.json" \
   './.venv/bin/python3 ./experiments/time/clusteronly.py {dist}'
