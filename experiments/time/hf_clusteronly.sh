380ff45a432901eb51580223ea445513ffcad025
jorisfu <joris.funk@student.hpi.uni-potsdam.de>
2026-09-16 11:25:04 +0200

    hyperfine results

--- experiments/time/hf_clusteronly.sh
+++ experiments/time/hf_clusteronly.sh
@@ -2,6 +2,6 @@
 hyperfine \
   --runs 50 \
   --warmup 3 \
-  -L dist euclidean,dixon_pds_sqeuclidean,nandist_euclidean,eirola_esd_mvn,eirola_esd_gmm,mesquita_eed \
+  -L dist eirola_esd_mvn,eirola_esd_gmm,mesquita_eed \
   --export-json "./results/time/hyperfine-clusteronly.json" \
   './.venv/bin/python3 ./experiments/time/clusteronly.py {dist}'
