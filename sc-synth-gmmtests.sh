d5d24a153813678b70b0f9afaa782c29ade59c3e
jorisfu <joris.funk@student.hpi.uni-potsdam.de>
2026-09-19 13:22:59 +0200

    sc script for gmm tests

---
+++ sc-synth-gmmtests.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -eux
+#SBATCH --job-name=synthetic_data_clustering
+#SBATCH --account=sci-renard
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=joris.funk@student.hpi.de
+#SBATCH --partition=cpu-batch # -p
+#SBATCH --cpus-per-task=128 # -c
+#SBATCH --mem=32gb
+#SBATCH --time=24:05:00
+#SBATCH --output=synth_gmmtests_%j.log # %j is job id
+
+eval "$(conda shell.bash hook)"
+conda activate chm-experiments
+python3 /sc/home/joris.funk/chm-supp/experiments/missingness/synthetic-gmmtests.py
