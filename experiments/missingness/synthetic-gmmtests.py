68d02bca73b6be16515b48d1503b2f17aac5c805
jorisfu <joris.funk@student.hpi.uni-potsdam.de>
2026-09-19 13:21:19 +0200

    fix gmmtest script

--- experiments/missingness/synthetic-gmmtests.py
+++ experiments/missingness/synthetic-gmmtests.py
@@ -5,11 +5,13 @@ if module_path not in sys.path:
     sys.path.append(module_path)
 
 from commontools import add_nans_uniform_everywhere
+from clusteredheatmap.algos.modelselection import get_best_gmm
 
 import time
 
 import runs
 import scipy
+import numpy as np
 
 from data.synthetic.get import get_dataset
 
@@ -17,12 +19,12 @@ synth_mat, true_fcluster, _ = get_dataset()
 
 MISSINGNISS_RATIOS = [i/100 for i in [10, 15, 20, 25, 30, 35, 40, 45, 50]]
 N_REPLICATES = 50
+rng = rng = np.random.default_rng(123)
 
-
-print("Missingness,Replicate,n_components")
+print("Missingness,Replicate,n_components", flush=True)
 for p in MISSINGNISS_RATIOS:
     for replicate in range(N_REPLICATES):
-        d_miss = add_nans_uniform_everywhere(synth_mat)
+        d_miss = add_nans_uniform_everywhere(synth_mat, p, rng)
         d_miss_cols = d_miss.transpose()
         gmm_cols = get_best_gmm(1, 4, 200, "BIC", d_miss_cols)
         print(str(p) + "," + str(replicate) + "," + str(gmm_cols.n_components), flush=True)
